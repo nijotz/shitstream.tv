c66a827747dd2b08345f4e85c17663933a35c986
Nick Tzaperas <nick@nijotz.com>
2015-05-11 18:23:04 +0000

    Prevent errors from files beginning with a dash

--- veejay.sh
+++ veejay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while true; do
-    ffmpeg -re -i "$(ls *.mp4 | shuf -n 1)" -c copy -f flv rtmp://localhost:1935/stream/live
+    ffmpeg -re -i "$(find -iname '*.mp4' | shuf -n 1)" -c copy -f flv rtmp://localhost:1935/stream/live
 done
