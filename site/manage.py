39d0c509476168f7c7ba202e98f2ada9b8063d2d
Bryan Clement <bclement@newrelic.com>
2015-05-20 17:03:13 -0700

    moved veejay into project folder

--- site/manage.py
+++ site/manage.py
@@ -4,13 +4,20 @@ from flask.ext.migrate import MigrateCommand
 from flask.ext.script import Manager, Server, Shell
 from flask.ext.script.commands import Clean, ShowUrls
 from shitstream import create_app, socketio
+from veejay import veejay
 
 app = create_app()
 manager = Manager(app)
 
+
 @manager.command
 def runserver():
     socketio.run(app, host=app.config.get('HOST'), port=app.config.get('PORT'))
+
+
+@manager.command
+def runveejay():
+    veejay.run()
 
 manager.add_command("shell", Shell())
 manager.add_command("clean", Clean())
---
+++ site/veejay/veejay.py
@@ -0,0 +1,55 @@
+#!/bin/python
+from random import random
+import os
+import subprocess
+from flask import current_app
+import psycopg2
+
+path = './'
+
+
+class entry:
+    def __init__(self, value, weight=1):
+        self.value = value
+        self.weight = weight
+
+    def __str__(self):
+        return str(self.value)
+
+
+def pick(entries):
+    total_weight = sum(map(lambda entry: entry.weight, entries))
+    choice_weight = total_weight*random()
+    chosen = None
+    i = 0
+
+    while choice_weight > 0:
+        chosen = entries[i]
+        choice_weight -= chosen.weight
+        i += 1
+
+    chosen.weight /= 10
+
+    for entry in entries:
+        entry.weight += 1
+
+    return chosen
+
+
+def run():
+    url_entries = []
+
+    for file_name in filter(lambda x: x[-3:] == 'mp4', os.listdir(path)):
+        url_entries.append(entry(path + file_name))
+
+    dburi = current_app.config['SQLALCHEMY_DATABASE_URI']
+    conn = psycopg2.connect(dburi)
+    curs = conn.cursor()
+
+    while True:
+        next_video = pick(url_entries)
+        curs.execute('NOTIFY queue;')
+        subprocess.call(['ffmpeg',
+                         '-re', '-i', str(next_video),
+                         '-c', 'copy',
+                         '-f', 'flv', 'rtmp://localhost:1935/stream/live'])
