a80d4b2e8b87daeed3c18c2a256dd74db094d2b5
Bryan Clement <bclement@newrelic.com>
2015-05-27 14:40:47 -0700

    moved weights and picking to models

--- site/shitstream/models.py
+++ site/shitstream/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import random
 from sqlalchemy.ext.declarative import declared_attr
 from shitstream import db
 
@@ -32,3 +33,32 @@ class Video(Base):
     key = db.Column(db.Text)
     filename = db.Column(db.Text)
     origin = db.Column(db.Text)
+
+
+class Weight(Base):
+    video_id = db.Column(db.Integer, db.ForeignKey('video.id'))
+    video = db.relationship('Video', backref=db.backref('weight',
+        lazy='dynamic'))
+    weight = db.Column(db.Integer)
+
+    def pick(self):
+        entries = self.query.all()
+        total_weight = sum(map(lambda entry: entry.weight, entries))
+        choice_weight = total_weight*random()
+        chosen = None
+        i = 0
+
+        while choice_weight > 0:
+            chosen = entries[i]
+            choice_weight -= chosen.weight
+            i += 1
+
+        chosen.weight //= 10
+
+        for entry in entries:
+            entry.weight += 1
+
+        db.session.add_all(entries)
+        db.session.commit()
+
+        return chosen.video
