e780045ac8101d4ce54e7719e13064465e48b218
Nick Tzaperas <nick@nijotz.com>
2015-06-12 18:09:28 -0700

    Use the shitstream user everywhere

--- site/config.py
+++ site/config.py
@@ -1,5 +1,5 @@
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgresql:///shitstream?user=vagrant'
+SQLALCHEMY_DATABASE_URI = 'postgresql:///shitstream?user=shitstream'
 HOST = '0.0.0.0'
 PORT = 5000
 MOVIE_DIR = '/var/www/shitstream/project/mp4s'
