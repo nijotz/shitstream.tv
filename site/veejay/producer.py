01e9b110bca0f919abc0f8b7e3c7d111df7e07f9
Nick Tzaperas <nick@nijotz.com>
2015-07-02 18:29:24 -0700

    Get filename from producer

--- site/veejay/producer.py
+++ site/veejay/producer.py
@@ -14,11 +14,11 @@ from shitstream.models import Video, Weight
 from shitstream.utils import temp_chdir
 
 
-def create_video_from_youtube(key, post):
+def create_video_from_youtube(key, filename, iff_post):
     vid = Video()
     vid.key = key
-    vid.filename = key + '.mp4'
-    origin_dict = { key : post[key] for key in ['person', 'title', 'created_at'] }
+    vid.filename = filename
+    origin_dict = { key : iff_post[key] for key in ['person', 'title', 'created_at'] }
     vid.origin = json.dumps(origin_dict)
     return vid
 
@@ -52,6 +52,16 @@ def youtube_key_from_url(url):
 def youtube_url_from_key(key):
     return 'http://www.youtube.com/watch?v=' + key
 
+
+def download_youtube_vid(url):
+    ytdl_cmd = 'youtube-dl --max-filesize 500M --id -i --download-archive shitstream-downloads "{}"'.format(url)
+    output = subprocess.check_output(ytdl_cmd, shell=True)
+    match = re.search('\[download\] Destination: (\S*)', output)
+    if match:
+        return match.group(1)
+    else:
+        return youtube_key_from_url(url) + '.fix'
+
 def get_new_videos():
     current_app.logger.info('Retrieving new youtube posts')
     iff_posts = get_iff_posts()[:100]
@@ -59,21 +69,27 @@ def get_new_videos():
     keyed_posts = { youtube_key_from_url(p['url']) : p for p in youtube_posts }
     existing_keys = [ v.key for v in Video.query.all() ]
     new_movie_keys = set(keyed_posts.keys()).difference(set(existing_keys))
-
     current_app.logger.info('New youtube keys: {}'.format(str(new_movie_keys)))
+
     for new_movie_key in new_movie_keys:
-        current_app.logger.info('Adding {} to the database'.format(new_movie_key))
-        new_video = create_video_from_youtube(new_movie_key, keyed_posts[new_movie_key])
-        weight = create_weights_for_videos(new_video)
-        db.session.add(new_video)
-        db.session.add(weight)
-        db.session.commit()
 
         current_app.logger.info('Downloading {}'.format(new_movie_key))
         movie_url = youtube_url_from_key(new_movie_key)
         with temp_chdir(current_app.config['MOVIE_DIR']):
-            ytdl_cmd = 'youtube-dl --max-filesize 500M --id -i --download-archive shitstream-downloads "{}"'.format(movie_url)
-            subprocess.call(ytdl_cmd, shell=True)
+            try:
+                filename = download_youtube_vid(movie_url)
+            except Exception as e:
+                current_app.logger.exception('Failed to download video', e)
+                continue
+
+        current_app.logger.info('Adding {} to the database'.format(new_movie_key))
+        new_video = create_video_from_youtube(new_movie_key, filename, keyed_posts[new_movie_key])
+        weight = create_weights_for_videos(new_video)
+
+        db.session.add(new_video)
+        db.session.add(weight)
+        db.session.commit()
+
 
 def run():
     current_app.logger.setLevel(logging.DEBUG)
